7242b45d67fe9d8b4b2b03a24d74ba09ff4c7c8f
dahci85 <darchiadachi3@gmail.com>
2026-05-16 14:05:42 +0400

    Added SQLAlchemy, Models, and Initial Migration

--- app.py
+++ app.py
@@ -1,16 +1,35 @@
 from flask import Flask, render_template, request, redirect, url_for
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 
 app = Flask(__name__)
 
 
-games = [
-    {'title': 'The Witcher 3', 'platform': 'PC', 'status': 'Completed'},
-    {'title': 'Elden Ring', 'platform': 'PS5', 'status': 'Playing'}
-]
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///game_nexus.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['SECRET_KEY'] = 'super-secret-key'
 
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
+
+
+class Game(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(100), nullable=False)
+    platform = db.Column(db.String(50), nullable=False)
+    status = db.Column(db.String(50), nullable=False)
+
+    def __repr__(self):
+        return f'<Game {self.title}>'
+
+
+# CRUD: Read (წაკითხვა)
 @app.route('/')
 def index():
-    return render_template('index.html', games=games)
+    games_list = Game.query.all()
+    return render_template('index.html', games=games_list)
+
+
 
 @app.route('/add', methods=['GET', 'POST'])
 def add_game():
@@ -18,9 +37,15 @@ def add_game():
         title = request.form.get('title')
         platform = request.form.get('platform')
         status = request.form.get('status')
-        games.append({'title': title, 'platform': platform, 'status': status})
+
+
+        new_game = Game(title=title, platform=platform, status=status)
+        db.session.add(new_game)
+        db.session.commit()
+
         return redirect(url_for('index'))
     return render_template('add_game.html')
+
 
 if __name__ == '__main__':
     app.run(debug=True)
